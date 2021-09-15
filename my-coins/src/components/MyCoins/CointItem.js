cc1fe41fca8e435027a193644e42dd5415946875
techitreesoutions <riteshnewal@hotmail.com>
2021-09-15 17:10:36 +0530

    coin item added

--- my-coins/src/components/MyCoins/CointItem.js
+++ my-coins/src/components/MyCoins/CointItem.js
@@ -8,11 +8,13 @@ export default class CointItem extends Component {
         
             <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
               <div class="box">
-                <div class="icon"><i class="bi bi-briefcase"></i></div>
+                <div class="icon"><i class="bi bi-currency-bitcoin"></i></div>
                 <h4 class="title"><a href="">{myCoin.Name}</a></h4>
-                <p class="description">{myCoin.Price}</p>
+                <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
               </div>
             </div>
+
+
     );
   }
 }
--- my-coins/src/components/MyCoins/MyCoinListing.js
+++ my-coins/src/components/MyCoins/MyCoinListing.js
@@ -14,41 +14,10 @@ export default class MyCoinListing extends Component {
         </div>
 
         <div class="row gy-4">
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <div class="box">
-              <div class="icon"><i class="bi bi-briefcase"></i></div>
-              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-            <div class="box">
-              <div class="icon"><i class="bi bi-card-checklist"></i></div>
-              <h4 class="title"><a href="">Dolor Sitema</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-            <div class="box">
-              <div class="icon"><i class="bi bi-bar-chart"></i></div>
-              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
-            <div class="box">
-              <div class="icon"><i class="bi bi-binoculars"></i></div>
-              <h4 class="title"><a href="">Magni Dolores</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum rideta zanox satirente madera</p>
-            </div>
-          </div>
-
+        {coinsData.map((item, index) => {
+                    return <CointItem myCoin={item} index={index} />;
+                  })}
         </div>
-
       </div>
     </section>
   
